fe1e42c581f9a4ca45e4e33da3597c6ba168b313
Francisco Soares <francisco.ssn94@gmail.com>
2026-06-08 11:56:16 -0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -190,7 +190,7 @@ int route_matches(const struct route *route, const char *path) {
 }
 
 void send_close_connection(int socket_fd) {
-  char *close_connection = "Connection: close";
+  char *close_connection = "HTTP/1.1\r\nConnection: close";
   log_send(socket_fd, close_connection, strlen(close_connection));
 }
 
