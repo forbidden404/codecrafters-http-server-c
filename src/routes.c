dc4202705fbfbd1ba813592df619c056ea4abc9a
Francisco Soares <francisco.ssn94@gmail.com>
2026-06-08 12:59:10 -0300

    codecrafters submit [skip ci]

--- src/routes.c
+++ src/routes.c
@@ -19,6 +19,7 @@ char *headers_body_to_field_line(Hashmap *headers, char *buffer,
 void add_connection_close_if_needed(const struct http_request *request,
                                     struct http_response_builder *builder) {
   bstring connection = Hashmap_get(request->headers, bfromcstr("Connection"));
+  printf("Connection: %s\n", bdata((bstring)connection));
   if (connection && bstrcmp(connection, bfromcstr("close")) == 0) {
     http_response_builder_option(builder, CLOSE);
   }
